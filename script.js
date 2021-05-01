0ab76ff601841746b54c86fd8321f008eb6c7a95
gabrielsr <gsrivillas0328@gmail.com>
2021-05-01 10:07:53 -0500

    Tipos de datos y declaracion, tipo let y const

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-const texto = document.getElementById("texto")
+// alert("!Soy JavaScript!")
+
+
+let mensaje;
+mensaje = "Hola mundo";
+
+// alert(mensaje);
+
+
+
+let a = 5;
+let b = 6;
+let suma = a+ b;
+console.log("El resultado de la suma es : " + suma);
+
+
+const PI = 3.14126;
+const RED_COLOR = "#FFFFF";
+
+pi_dos = pi;
+pi_dos = 7;
+console.log(PI);
+console.log(pi_dos);
+
+
+
+let mensaje = "Hola";
+mensaje = 1232;
+
+
+//Tipos de datos 
+//numero
+let n = 1222;
+n = 12.5434;
+
+// const texto = document.getElementById("texto")
+
+// function cambioTexto(){
+//     texto.innerHTML = "!Soy JavaScript!"
+// }
